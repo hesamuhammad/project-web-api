c76a468261e1c11379ac508a00f280cb8a2292a3
Rahmat Fauzi <rhfauzi7@gmail.com>
2020-01-29 15:57:58 +0700

    push error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Header from './components/Header';
+import Bebas from './pages/Bebas';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Mock from './pages/Mock';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
 
 function App() {
+  const isLogin = JSON.parse(localStorage.getItem('status'));
   return (
-    <div className="App">
-      <h1>Tim HESA and Fauzi</h1>
-    </div>
+    <Router>
+      <Header />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/login">
+          <Login />
+        </Route>
+
+        <Route exact path="/register">
+          <Register />
+        </Route>
+
+        {isLogin ? (
+          <Fragment>
+            <Route exact path="/profile/">
+              <Profile />
+            </Route>
+
+            <Route exact path="/mock/">
+              <Mock />
+            </Route>
+
+            <Route exact path="/bebas/">
+              <Bebas />
+            </Route>
+          </Fragment>
+        ) : (
+            <Redirect to="/login" />
+          )}
+
+      </Switch>
+    </Router>
   );
 }
 
