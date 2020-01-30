b9a1ebb4a3186db262f4b1e81d617c4443e58eaf
Rahmat Fauzi <rhfauzi7@gmail.com>
2020-01-30 09:10:27 +0700

    Update Fauzi

--- src/pages/Bebas.js
+++ src/pages/Bebas.js
@@ -16,22 +16,19 @@ import axios from "axios";
 export default function Bebas() {
   const [allData, setAllData] = useState([]);
   const [name, setName] = useState("");
-  const [total, setTotal] = useState(0);
-  const [loading, setLoading] = useState(true);
+  const urlAll = "https://api.unsplash.com/photos/?client_id=ad95db50f014a2929e44bbc127634c9f56f31e3ee7c81f1bc7dc2096370d6d09";
 
   useEffect(() => {
     getData();
-  }, [total]);
+  }, []);
 
   const getData = () => {
     axios
-      .get("https://5e2fe92f9c29c900145db5c1.mockapi.io/testpost")
+      .get(urlAll)
       .then(response => {
         if (response.status === 200) {
           const newData = response.data;
           setAllData(newData);
-          setLoading(false);
-          setTotal(newData.length);
           // console.log(data);
         } else {
           console.log("Something is wrong with that status data");
@@ -42,10 +39,11 @@ export default function Bebas() {
 
   const handleRemove = index => {
     const newId = index;
-    const url = `https://5e2fe92f9c29c900145db5c1.mockapi.io/testpost/`;
+    const url = `${urlAll}/`;
     axios
       .delete(url + newId)
       .then(res => {
+        getData();
         console.log(res.data);
       })
       .catch(err => {
@@ -57,8 +55,10 @@ export default function Bebas() {
 
   const handleUpdate = index => {
     const newName = prompt("New Name : ");
-    // alert(index);
-    const url = `https://5e2fe92f9c29c900145db5c1.mockapi.io/testpost/${index}`;
+    if (newName === null) {
+      return
+    }
+    const url = `${urlAll}/${index}`;
     const user = {
       id: index,
       name: newName
@@ -66,9 +66,12 @@ export default function Bebas() {
 
     axios
       .put(url, user)
-      .then(res => console.log(res.data))
-      .then(error => {
-        console.log(error);
+      .then(res => {
+        getData();
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
       });
 
     console.log(allData);
@@ -77,23 +80,33 @@ export default function Bebas() {
   const handleUpdateImages = index => {
     const newAvatar = prompt("URL Images : ");
     // alert(index);
-    const url = `https://5e2fe92f9c29c900145db5c1.mockapi.io/testpost/${index}`;
+    const url = `${urlAll}/${index}`;
     const user = {
       avatar: newAvatar
     };
 
-    axios.put(url, user).then(res => console.log(res.data));
+    axios.put(url, user)
+      .then(res => {
+        getData();
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
     console.log(allData);
   };
 
+  const handleChange = e => {
+    setName(e.target.value);
+    console.log(e.target.value);
+  }
+
   const handleSubmit = event => {
     event.preventDefault();
-    const newNama = prompt("Nama Anda");
-
     axios
-      .post(`https://5e2fe92f9c29c900145db5c1.mockapi.io/testpost`, {
-        name: newNama
+      .post(`${urlAll}`, {
+        name: name
       })
       .then(res => {
         getData();
@@ -107,74 +120,45 @@ export default function Bebas() {
 
   return (
     <Container style={{ marginTop: "20px" }}>
-      {!loading ? (
-        <Fragment>
-          <Col md="12" xs="12">
-
-            <form onSubmit={this.handleSubmit}>
-              <input
-                type="text"
-                value={name}
-                onChange={this.handleChange}
-                placeholder="Put your name here"
-                style={{}} />
-              <button
-                type="submit"
-              >
-                input
-                        </button>
-            </form>
-            <Button color="success" onClick={handleSubmit}>
-              Add
-            </Button>
-
+      <Fragment>
+        <Col md="12" xs="12">
+          <form onSubmit={handleSubmit} className='mt-4'>
             <Row>
-              {allData.map((item, index) => {
-                return (
-                  <Col key={index} md="3" sm="6" xs="12" className="cardz">
-                    <Card>
-                      <CardImg
-                        top
-                        width="100%"
-                        src={item.avatar}
-                        alt="Card image cap"
-                      />
-                      <CardBody>
-                        <CardTitle>{item.name}</CardTitle>
-                        <CardSubtitle>{item.id}</CardSubtitle>
-                        <CardText>
-                          Some quick example text to build on the card title and
-                          make up the bulk of the card's content.
-                        </CardText>
-                        <Button
-                          color="success"
-                          onClick={() => handleUpdate(item.id)}
-                        >
-                          Edit
-                        </Button>
-                        <Button
-                          color="primary"
-                          onClick={() => handleUpdateImages(item.id)}
-                        >
-                          Edit Img
-                        </Button>
-                        <Button
-                          color="danger"
-                          onClick={() => handleRemove(item.id)}
-                        >
-                          Remove
-                        </Button>
-                      </CardBody>
-                    </Card>
-                  </Col>
-                );
-              })}
+              <Col md={12}>
+                <input className='form-control' type='text' name='name' value={name} onChange={handleChange} placeholder='Your Name' />
+              </Col>
             </Row>
-          </Col>
-        </Fragment>
-      ) : (
-          <p className="text-center">Loading</p>
-        )}
+            <button className='btn btn-success btn-block mt-2' type="submit">POST</button>
+          </form>
+
+          <Row>
+            {allData.map((item, index) => {
+              return (
+                <Col key={index} md="3" sm="6" xs="12" className="cardz">
+                  <Card>
+                    <CardImg top width="100%" src={item.urls.small} alt="Card image cap" />
+                    <CardBody>
+                      <CardTitle>{item.description}</CardTitle>
+                      <CardSubtitle>{item.alt_description}</CardSubtitle>
+                      <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+
+                      {/* <Button color="success" onClick={() => handleUpdate(item.id)} > Edit </Button>
+                      <Button color="primary" onClick={() => handleUpdateImages(item.id)} >Edit Img</Button>
+                      <Button
+                        color="danger"
+                        onClick={() => handleRemove(item.id)}
+                      >
+                        Remove
+                        </Button> */}
+                    </CardBody>
+                  </Card>
+
+                </Col>
+              );
+            })}
+          </Row>
+        </Col>
+      </Fragment>
     </Container>
   );
 }
